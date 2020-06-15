2272154a61f4b4b194228b8ee833da50f29dedb5
Penguin <jeremy@jeremytanner.com>
2020-06-14 23:06:28 -0500

    Change right enter to right shift

--- keyboards/rgbkb/sol/keymaps/penguin/keymap.c
+++ keyboards/rgbkb/sol/keymaps/penguin/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|------|  |------|------+------+------+------+------+------|
    * |FN(CAPS)| A  |   S  |   D  |   F  |   G  |   {  |  |   }  |   H  |   J  |   K  |   L  |   ;  |   '  |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-   * |Shift |   Z  |   X  |   C  |   V  |   B  |   (  |  |   )  |   N  |   M  |   ,  |   .  |   /  |Enter |
+   * |Shift |   Z  |   X  |   C  |   V  |   B  |   (  |  |   )  |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
    * | Left |  Up  |  ADJ |  RGB |  FN  | Cmd  | BkSp |  | Enter| Space|  Cmd | Crtl | Alt  | Down |Right |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------'
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MINS,  KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL, \
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC, KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS, \
     FN_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LCBR, KC_RCBR,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, \
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN, KC_RPRN,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT, \
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LPRN, KC_RPRN,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, \
     KC_LEFT,   KC_UP,     ADJ, RGB_TOG,      FN, KC_LGUI, KC_BSPC,  KC_ENT,  KC_SPC, KC_LGUI, KC_LCTL, KC_LALT, KC_DOWN,KC_RIGHT, \
                                                  KC_LGUI, KC_BSPC,  KC_ENT,  KC_SPC \
   ),
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|------|  |------|------+------+------+------+------+------|
    * |FN(CAPS)| A  |   R  |   S  |   T  |   G  |   (  |  |   )  |   K  |   N  |   E  |   I  |   O  |   '  |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-   * |Shift |   Z  |   X  |   C  |   D  |   V  |   {  |  |   }  |   M  |   H  |   ,  |   .  |   /  |Enter |
+   * |Shift |   Z  |   X  |   C  |   D  |   V  |   {  |  |   }  |   M  |   H  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
    * | Ctrl |  Win |  Alt |  RGB | ADJ  | Space| DEL  |  | Enter| Space|  FN  | Left | Down | Up   |Right |
    * |------+------+------+------+------+------+------|  |------+------+------+------+------+------+------'
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MINS,  KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, \
      KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G, KC_LBRC, KC_RBRC,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSLS, \
     FN_CAPS,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D, KC_LPRN, KC_RPRN,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT, \
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LCBR, KC_RCBR,    KC_K,    KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT, \
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_LCBR, KC_RCBR,    KC_K,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
     KC_LCTL, KC_LGUI, KC_LALT, RGB_TOG,     ADJ,  KC_SPC,  KC_DEL,  KC_ENT,  KC_SPC,      FN, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, \
                                                   KC_SPC,  KC_DEL,  KC_ENT,  KC_SPC \
   ),
